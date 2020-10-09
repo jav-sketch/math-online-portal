75c69fa539e23d08acb2d36e51101e8dcb5f6b14
[jav-sketch] <[javonbrowne@gmail.com]>
2020-10-09 19:54:32 -0400

    updated models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    CATEGORY = (
+        ('FT', 'FULL TIME'),
+        ('AS', 'AFTER SCHOOL'),
+        ('EC', 'EVENING CLASSES'),
+        ('PT', 'PRIVATE TUTORING'),
+        ('SC', 'SATURDAY CLASSES'),
+        ('SC', 'SPECIALIZED COURSES'),
+    )
+
+    CATEGORY_CHOICES = (
+        ('N', 'NONE'),
+        ('PT', 'PART-TIME'),
+        ('F', 'FULL-TIME'),
+    )
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField()
+    category = models.CharField(choices=CATEGORY, max_length=20)
+    evening = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
+    price = models.FloatField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
