a64d471acfa021e6ac66d204c451245634a9cae3
[jav-sketch] <[javonbrowne@gmail.com]>
2020-10-09 19:43:30 -0400

    Added Allauths

--- math_portal/urls.py
+++ math_portal/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('core/', include("core.urls")),
+    path('accounts/', include("allauth.urls")),
     path('admin/', admin.site.urls),
 ]
