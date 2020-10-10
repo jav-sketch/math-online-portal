761ae59fc74660bd96e9ff29843eb07fa32620ed
[jav-sketch] <[javonbrowne@gmail.com]>
2020-10-10 17:41:45 -0400

    Test clientside

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+# from .views import HomeView
 from . import views
+
+
 appname = 'core'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # path('', HomeView.as_view(), name="index"),
 ]
