f4b94d3fa184fe353a40d4b9a26f76aba46a194b
[jav-sketch] <[javonbrowne@gmail.com]>
2020-10-20 10:20:07 -0400

    Updated views

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,10 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('course/', CourseView.as_view(), name="course"),
-    path('detail/<slug>/', CourseDetailView.as_view(), name="detail"),
-    path('enroll/<slug>/', EnrollDetailView.as_view(), name='enroll'),
+    path('course/<slug>/', CourseView.as_view(), name="course"),
+    # path('detail/<slug>/', CourseDetailView.as_view(), name="detail"),
+    path('enroll/<slug>/', EnrollDetailView.as_view(), name='enroll-summary'),
+    path('add-course/<slug>/', add_course, name='add-course'),
+    path('remove-course/<slug>/', remove_course, name='remove-course'),
+    path('remove-single-course/<slug>/', remove_single_course_item, name='remove-single-course'),
 ]
