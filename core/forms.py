c7ccf152127538d3acc32d2d141d5fdbbe101947
[jav-sketch] <[javonbrowne@gmail.com]>
2020-11-04 15:09:46 -0400

    Updated forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
 
-class checkout(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
-    country = CountryField()
+PAYMENT_CHOICES = (
+    ('Stripe', 'Stripe'),
+    ('Paypal', 'Paypal'),
+    ('MMG', 'MMG'),
+)
+class CheckoutForm(forms.Form):
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '123 Main st'
+    }))
+
+    address_2 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or Suite'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
+        'class': 'custom-select d-block w-100'
+    }))
     zip = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput)
-    save_info = forms.BooleanField(widget=forms.CheckboxInput)
-    payment_option = forms.BooleanField(widget=forms.RadioSelect)
+    same_billing_address = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    save_info = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    payment = forms.ChoiceField(widget=forms.RadioSelect,choices=PAYMENT_CHOICES)
