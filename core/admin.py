2bf796bc292be1b0f0a74cf8ee23913d58ee1bac
[jav-sketch] <[javonbrowne@gmail.com]>
2020-11-23 16:38:04 -0400

    Updated admin

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import *
-
+class EnrollAdmin(admin.ModelAdmin):
+    list_display = ['user', 'enrolled']
+    
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(CourseItem)
 admin.site.register(Teacher)
-admin.site.register(Enroll)
+admin.site.register(Enroll, EnrollAdmin)
 admin.site.register(Heading)
 admin.site.register(Section)
 admin.site.register(BillingAddress)
 admin.site.register(Payment)
+admin.site.register(Coupon)
