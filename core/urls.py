4c030dd5f6f44dc355b9b443357f13f736232b14
[jav-sketch] <[javonbrowne@gmail.com]>
2020-11-26 16:58:18 -0400

    Added additional URL configs

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,8 @@ from django.views.generic.base import RedirectView
 
 app_name = 'core'
 urlpatterns = [
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('img/favicon.ico'))),
     path('', HomeView.as_view(), name='index'),
     path('course/', CourseView.as_view(), name="course"),
     path('course/<slug>/', CourseView.as_view(), name="course"),
@@ -15,11 +16,11 @@ urlpatterns = [
     # path('enroll/<slug>/', EnrollDetailView.as_view(), name='enroll-summary'),
     path('enroll-summary/', EnrollSummaryView.as_view(), name='enroll-summary'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('add-coupon/',
+         AddCouponView.as_view(), name='add-coupon'),
     path('add-course/<slug>/', add_course, name='add-course'),
     path('remove-course/<slug>/', remove_course, name='remove-course'),
     path('payment/<payment>/', PaymentView.as_view(), name='payment'),
     # path('remove-single-course/<slug>/',
     #      remove_single_course_item, name='remove-single-course'),
-    path('add_coupon/<code>/',
-        add_coupon, name='add_coupon'),
 ]
