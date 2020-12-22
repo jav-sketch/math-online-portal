d73d7fe9fbb11e275cb5b430434302e19b7e46f1
[jav-sketch] <[javonbrowne@gmail.com]>
2020-12-22 17:19:37 -0400

    Added request refund form

--- core/forms.py
+++ core/forms.py
@@ -32,3 +32,9 @@ class CouponForm(forms.Form):
         'aria-label:': 'Recipient\'s username',
         'aria-describedby': 'basic-addon2',
     }))
+
+class RefundForm(forms.Form):
+    ref_code = forms.CharField()
+    message = forms.Textarea()
+    email = forms.EmailField()
+
