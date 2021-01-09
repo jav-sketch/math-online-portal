2133df2a8a9bfd8412969e20db744986072de227
[jav-sketch] <[javonbrowne@gmail.com]>
2021-01-09 18:15:16 -0400

    Create custom admin model for address

--- core/admin.py
+++ core/admin.py
@@ -11,17 +11,34 @@ make_refund_accepted.short_description = 'Update enrolls to refund granted'
 class EnrollAdmin(admin.ModelAdmin):
     list_display = ['user', 'enrolled',
                     'refund_requested', 'refund_granted',
-                    'payment', 'billing_address', 'coupon']
+                    'payment', 'billing_address', 'shipping_address', 'coupon']
 
     list_display_links = ['user', 'payment',
-                          'billing_address',  'coupon']
+                          'billing_address', 'shipping_address', 'coupon']
 
     list_filter = ['enrolled', 'refund',
                    'refund_requested', 'refund_granted']
 
     search_fields = ['user__username',
                      'ref_code', ]
-    actions = [make_refund_accepted]                 
+    actions = [make_refund_accepted] 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'address',
+        'address_2',
+        'country',
+        'zip',
+        'address_type',
+        'default',
+    ]           
+
+    list_filter = [
+        'default',
+        'address_type',
+        'country',
+    ]
+    search_fields = ['user', 'address', 'address_2', 'zip']         
 
 
 # Register your models here.
@@ -31,7 +48,7 @@ admin.site.register(Teacher)
 admin.site.register(Enroll, EnrollAdmin)
 admin.site.register(Heading)
 admin.site.register(Section)
-admin.site.register(BillingAddress)
+admin.site.register(Address, AddressAdmin)
 admin.site.register(Payment)
 admin.site.register(Coupon)
 admin.site.register(Refund)
