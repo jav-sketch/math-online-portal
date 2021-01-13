46ab52ce1d41dfafdde9cda6d8d83c168f7af1fc
[jav-sketch] <[javonbrowne@gmail.com]>
2021-01-13 17:54:48 -0400

    added payment form to forms

--- core/forms.py
+++ core/forms.py
@@ -56,3 +56,8 @@ class RefundForm(forms.Form):
     }))
     email = forms.EmailField()
 
+class PaymentForm(forms.Form):
+    stripeToken = forms.CharField(required=False)    
+    save = forms.BooleanField(required=False)    
+    use_default = forms.BooleanField(required=False)    
+
